7d68357bd45419d4add91e46fde09cb024901346
Applecakeman <sebastian.baier@live.de>
2021-08-26 17:54:59 +0200

    -prepare for production

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors);
 app.use('/oauth/access_token', tokens);
+require('./middleware/prod')(app);
 
 const options = {
     key: fs.readFileSync('cert/localhost.key'),
